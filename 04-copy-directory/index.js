233d80547ce4fe8afb0b3b1f5084120423c5ce56
Vi-user <vi.volkova.2712@gmail.com>
2022-11-01 17:07:05 +0300

    feat: add solution to 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+const curFolder = path.join(__dirname, 'files');
+const copyFolder = path.join(__dirname, 'files-copy');
+
+async function clearDir(folder) {
+  try{
+    const files = await fs.readdir(folder);
+    const deleteFiles = files.map(file => fs.unlink(path.join(folder, file)))
+    return Promise.all(deleteFiles)
+  } catch (err) {
+    console.error(`Error: ${err}`)
+  }
+}
+
+
+async function copyDir() {
+  try {
+    const copiedDir = await fs.mkdir(copyFolder, {recursive: true});
+    const clearNewDir = await clearDir(copyFolder);
+    const curFolderFiles = await fs.readdir(curFolder);
+    curFolderFiles.forEach(file => {
+      const curFile = path.join(curFolder, file);
+      const newFile = path.join(copyFolder, file);
+      fs.copyFile(curFile, newFile);
+    })
+    console.log(`files directory were successfully copied into files-copy dir` )
+  } catch (err) {
+    console.error(`Error: ${err}`)
+  }
+}
+
+copyDir();
+
+
 
